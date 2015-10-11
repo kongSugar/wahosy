93f82b85c8883b82311ea8703591df0fa5624069
KongSugar <niko@kongsugar.de>
2015-10-11 13:32:05 +0200

    fixes

--- src/main/java/de/kongsugar/calendar/CalendarMonth.java
+++ src/main/java/de/kongsugar/calendar/CalendarMonth.java
@@ -3,14 +3,13 @@ package de.kongsugar.calendar;
 /**
  * Created by Niko on 07.10.2015.
  */
-public class CalendarMonth extends Calendar {
+public class CalendarMonth   {
 
 
     public CalendarMonth() {
         //Load FXML
     }
 
-    @Override
     public void show() {
 
     }
---
+++ src/main/java/de/kongsugar/wahosy/Sample.java
@@ -0,0 +1,13 @@
+package de.kongsugar.wahosy;
+
+import de.kongsugar.wahosy.model.dao.ItemDAO;
+import de.kongsugar.wahosy.model.dao.batis.DbItem;
+
+/**
+ * Created by nikog on 11.10.2015.
+ */
+public class Sample {
+    public static void main(String[] args) {
+
+    }
+}
